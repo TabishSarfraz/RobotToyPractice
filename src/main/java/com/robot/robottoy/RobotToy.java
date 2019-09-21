1b32af23081c49512e296b4ccd7a56bceaedefe6
tabishsarfraz <tabishsarfraz@live.com>
2019-09-21 13:45:01 +1000

    First commit setup project and working Place command

---
+++ src/main/java/com/robot/robottoy/RobotToy.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.robot.robottoy;
+
+/**
+ *
+ * @author Tabish
+ */
+public class RobotToy {
+    
+    public RobotToy(){
+    
+    }
+    
+    public RobotToy(int positionX, int positionY, String faceDirection){
+        
+        
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.faceDirection = faceDirection;
+        
+    }
+    
+    private int positionX;
+    private int positionY;
+    private String faceDirection;
+    private boolean onTable; 
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+
+    public String getFaceDirection() {
+        return faceDirection;
+    }
+
+    public void setFaceDirection(String faceDirection) {
+        this.faceDirection = faceDirection;
+    }
+
+    public boolean isOnTable() {
+        return onTable;
+    }
+
+    public void setOnTable(boolean onTable) {
+        this.onTable = onTable;
+    }
+
+    @Override
+    public String toString() {
+        return "RobotToy{" + "positionX=" + positionX + ", positionY=" + positionY + ", faceDirection=" + faceDirection + ", onTable=" + onTable + '}';
+    }
+    
+}
---
+++ src/main/java/com/robot/robottoy/RobotToyApp.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.robot.robottoy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author Tabish
+ */
+public class RobotToyApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        
+        RobotToy robotOne = new RobotToy();
+        
+        int tableXLength = 5;
+        int tableYLength = 5;
+        
+        int tableMaxLengthX = tableXLength - 1;
+        int tableMaxLengthY = tableYLength - 1;
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        Pattern pattern = Pattern.compile("^PLACE\\s\\d,\\d,(NORTH|SOUTH|EAST|WEST)");
+        
+        while(userInput.hasNextLine()){
+            
+            String userCommand = userInput.nextLine();
+            //System.out.println("userInput is :" + userInput.nextLine());
+            
+            System.out.println("userCommand is :" + userCommand);
+            
+            Matcher matcher = pattern.matcher(userCommand.trim());
+            
+           if(matcher.matches()){
+               
+               System.out.println("MATCEHR FOUND is :" + matcher);
+               
+               String[] placeCommandHolder = userCommand.split("\\s");
+               
+               int postX = Integer.parseInt(placeCommandHolder[1]);
+               int postY = Integer.parseInt(placeCommandHolder[2]);
+               String directionValue = placeCommandHolder[3];
+               
+           }
+            
+        }
+        
+    }
+    
+}
